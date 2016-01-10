217de9a15ec1a9b994ca7a5e4569f76c08b4f053
Victor Wang <wangshengjia01@gmail.com>
2016-01-10 23:34:55 +0100

    Generate the example project

---
+++ Example/Pods/Target Support Files/LeeGo/LeeGo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LeeGo : NSObject
+@end
+@implementation PodsDummy_LeeGo
+@end
---
+++ Example/Pods/Target Support Files/Pods-LeeGo_Example/Pods-LeeGo_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LeeGo_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LeeGo_ExampleVersionString[];
+
