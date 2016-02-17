5f30fff39490d093808e3b6c488eae430dc0ad8f
Victor Wang <wangshengjia01@gmail.com>
2016-02-17 14:26:24 +0100

    Run pod install

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,54 +2,57 @@
 
 import Quick
 import Nimble
-import LeeGo
+@testable import LeeGo
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
 
-        describe("") { () -> Void in
-            
+        describe("ComponentTarget tests") { () -> Void in
+            it("should create a new component target object with correct name & type", closure: { () -> () in
+                let component = ComponentTarget(name: "title", targetClass: UILabel.self)
+                expect(component.name) == "title"
+            })
         }
 
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
-        }
+//        describe("these will fail") {
+//
+//            it("can do maths") {
+//                expect(1) == 2
+//            }
+//
+//            it("can read") {
+//                expect("number") == "string"
+//            }
+//
+//            it("will eventually fail") {
+//                expect("time").toEventually( equal("done") )
+//            }
+//            
+//            context("these will pass") {
+//
+//                it("can do maths") {
+//                    expect(23) == 23
+//                }
+//
+//                it("can read") {
+//                    expect("🐮") == "🐮"
+//                }
+//
+//                it("will eventually pass") {
+//                    var time = "passing"
+//
+//                    dispatch_async(dispatch_get_main_queue()) {
+//                        time = "done"
+//                    }
+//
+//                    waitUntil { done in
+//                        NSThread.sleepForTimeInterval(0.5)
+//                        expect(time) == "done"
+//
+//                        done()
+//                    }
+//                }
+//            }
+//        }
     }
 }
