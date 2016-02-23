fd098bcb9811dc81ec845c7b457ed43690bdb907
Victor Wang <wangshengjia01@gmail.com>
2016-02-23 12:44:00 +0100

    Remove unnecessary pods and tests

---
+++ Demo/Pods/Target Support Files/Pods-LeeGo-Demo/Pods-LeeGo-Demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeeGo_Demo : NSObject
+@end
+@implementation PodsDummy_Pods_LeeGo_Demo
+@end
--- Demo/Pods/Target Support Files/Pods-LeeGo_Example/Pods-LeeGo_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_LeeGo_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_LeeGo_ExampleVersionString[];
-
