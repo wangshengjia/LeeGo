ea1543f619a23e50f3dbf05457f8b8df877737af
Victor Wang <wangshengjia01@gmail.com>
2016-03-05 11:04:23 +0100

    Remove `owner` & `viewName`

--- Demo/LeeGo/ElementViewModel.swift
+++ Demo/LeeGo/ElementViewModel.swift
@@ -29,16 +29,16 @@ extension ElementViewModel {
 extension ElementViewModel: ComponentDataSource {
     func updateComponent(componentView: UIView, with componentTarget: ComponentTarget) {
         switch componentView {
-        case let titleLabel as UILabel where componentView.name == String(ComponentBuilder.title):
+        case let titleLabel as UILabel where componentView.componentName == String(ComponentBuilder.title): // TODO: Add isComponent:
             titleLabel.setAttributeString(with: [
                 Style.marker: element.isRestrict ? "󰀀" : "",
                 Style.customTitle: element.title ?? "",
                 Style.nature: element.natureEdito ?? ""
                 ])
 
-        case let subtitleLabel as UILabel  where componentView.name == String(ComponentBuilder.subtitle):
+        case let subtitleLabel as UILabel  where componentView.componentName == String(ComponentBuilder.subtitle):
             subtitleLabel.text = element.description
-        case let avatar as UIImageView where componentView.name == String(ComponentBuilder.avatar):
+        case let avatar as UIImageView where componentView.componentName == String(ComponentBuilder.avatar):
             avatar.backgroundColor = UIColor.grayColor()
         default:
             break
